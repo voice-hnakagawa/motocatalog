ffd662c15aff55eec0c4ed22290f7a037529e85f
Hunter Nakagawa <h.nakagawa@i-voice.co.jp>
2026-04-23 14:55:55 +0900

    fix: add transactional rollback

--- src/main/java/ooo/klae/sample/motocatalog/services/MotosService.java
+++ src/main/java/ooo/klae/sample/motocatalog/services/MotosService.java
@@ -3,6 +3,7 @@ package ooo.klae.sample.motocatalog.services;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import ooo.klae.sample.motocatalog.beans.Motorcycle;
 import ooo.klae.sample.motocatalog.mappers.MotorcycleMapper;
@@ -40,11 +41,18 @@ public class MotosService {
      * @param motorcycle 更新するバイク情報
      * @return 更新件数
      */
+    @Transactional
     public int save(Motorcycle motorcycle) {
         int cnt = motorcycleMapper.update(motorcycle);
         if (cnt == 0) {
             throw new OptimisticLockingFailureException("楽観的なロックに失敗しました。データは他のユーザーによって更新されました。");
         }
+
+        // 2件以上更新された場合は、データの整合性に問題があるため例外をスローする
+        if (cnt > 1) {
+            throw new RuntimeException("複数件更新されました。データの整合性に問題があります。");
+        }
+
         return cnt;
     }
 }
